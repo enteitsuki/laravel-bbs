59f48d9c117d283c846ef5fd0862ab4050bfcb42
enteitsuki <enteitsuki@gmail.com>
2021-03-26 06:21:28 +0900

    モデル・コントローラー作成

--- server/routes/web.php
+++ server/routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', "BbsEntryController@index");
+Route::post('/create', "BbsEntryController@create");
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
