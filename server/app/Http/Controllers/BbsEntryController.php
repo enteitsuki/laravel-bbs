c6a04cac017f1800448401304c13592f6dd5ea5f
enteitsuki <enteitsuki@gmail.com>
2021-03-26 22:45:48 +0900

    バリデーション設定

--- server/app/Http/Controllers/BbsEntryController.php
+++ server/app/Http/Controllers/BbsEntryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
 use App\BbsEntry;
 
 class BbsEntryController extends Controller
@@ -12,7 +13,7 @@ class BbsEntryController extends Controller
         // $item_list = BbsEntry::all();
 
         // 並び順を指定
-        $item_list = BbsEntry::orderBy("id", "desc")->get();
+        $item_list = BbsEntry::orderBy("id", "desc")->paginate(10);
         return view("bbs_entry_list", [
             "item_list" => $item_list
         ]);
@@ -21,6 +22,19 @@ class BbsEntryController extends Controller
     {
         $input = $request->only('author', 'title', 'body');
 
+        $validator = Validator::make($input, [
+            'author' => 'required|string|max:30',
+            'title' => 'required|string|max:30',
+            'body' => 'required|string|max:100'
+        ]);
+
+        //バリデーション失敗
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withErrors($validator);
+        }
+		
+		//バリデーション成功
         $entry = new BbsEntry();
         $entry->author = $input["author"];
         $entry->title = $input["title"];
